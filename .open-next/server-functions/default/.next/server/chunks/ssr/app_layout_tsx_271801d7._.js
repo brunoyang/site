5f528960647e4350e8c0adc81ba86cdc97c2b8b9
brunoyang <ygch1993@gmail.com>
2026-02-07 22:15:07 +0800

    rm open-next

--- .open-next/server-functions/default/.next/server/chunks/ssr/app_layout_tsx_271801d7._.js
+++
@@ -1,3 +0,0 @@
-module.exports=[33290,a=>{"use strict";var b=a.i(89138);function c({children:a}){return(0,b.jsx)("html",{lang:"zh-CN",children:(0,b.jsx)("body",{className:"antialiased",children:a})})}a.s(["default",()=>c,"metadata",0,{title:"Cloudflare Next.js Demo",description:"Learn how to deploy Next.js on Cloudflare Pages"}])}];
-
-//# sourceMappingURL=app_layout_tsx_271801d7._.js.map
--- .open-next/server-functions/default/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"IDRa6keRWRYSCvYAbe0+Gr3tocxgEEb26hK3FTboIIg=\"\n}"
